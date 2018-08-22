c6bb8f76f8e0b0dcb4d0fc8bd35ad83a8e8760f7
Kociak1 <42005327+Kociak1@users.noreply.github.com>
2018-08-22 11:50:09 +0200

    Update HomeServlet.java

--- src/main/java/com/javamaster/controller/HomeServlet.java
+++ src/main/java/com/javamaster/controller/HomeServlet.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
+import static javax.servlet.http.HttpServlet;
+import static javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
